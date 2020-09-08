9267b53a11e40ce0ff89f3fa902d3be8e25f5e5b
Bayron <bayron_fabian@hotmail.com>
2020-09-07 21:34:34 -0300

    Add .input in styles and new classname in app.js

--- App.js
+++ App.js
@@ -1,7 +1,29 @@
-import React    from "react";
+import React ,{useState}   from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
 
 function App(){
+    const [data, setData] = useState("");
+    const calcBtns = [];
+    [9, 8, 7, 6, 5, 4, 3, 2, 1, 0, ".", "%"].forEach((item) => {
+        calcBtns.push(
+          <button
+            onClick={(e) => { setInput(input + e.target.value); }} value={item} key={item}>
+            {" "}
+            {item}
+          </button>
+           );
+        });
+    return (
+        <div className="outline">
+        <div className="input">
+          {data}
+        </div>  
+        <div className="digits">
+          </div>  
+        </div>
+    );
+}
 
-} 
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # ExerciseApp
 Exercise For 23People. 
 -----------------------------------------
-v.0   - First Commit for separe branch and organize folders
-v.0.1 - import React, React-dom and styles.css for for use
+v.0   - First Commit for separe branch and organize folders.
+v.0.1 - import React, React-dom and styles.css for use.
+v.0.2 - Add the necesary to make first hello world. 
+        Add useState for the interaction with buttons
+        Add .outline in Styles.css
 
