59af20468fcc3202bccc17dade7b95ef2948938f
Bayron <bayron_fabian@hotmail.com>
2020-09-09 15:35:20 -0300

    spelling changes

--- Calculator.js
+++ Calculator.js
@@ -1,48 +1,48 @@
-import React ,{useState}   from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
 
-function Calculator(){
-    const [data, setData] = useState("");
-    const calcBtns = [];
-    [9, 8, 7, 6, 5, 4, 3, 2, 1, 0, ".", "%"].forEach((item) => {
-        calcBtns.push(
-          <button
-            onClick={(e) => { setData(data + e.target.value); }} value={item} key={item}>
-            {" "}
-            {item}
-          </button>
-           )
-        })
-    return (
-    <div className="outline"> {" "}
-        <div className="input">
-          {data}
-        </div>  
-        <div className="digits">
-          {calcBtns}
-        </div>
-        {/* First Grid */}
-      <div className="grid">
-        <button onClick={()=> setData(data.substr(0,data.length - 1))}>
+function Calculator() {
+  const [data, setData] = useState("");
+  const calcBtns = [];
+  [9, 8, 7, 6, 5, 4, 3, 2, 1, 0, ".", "%"].forEach((item) => {
+    calcBtns.push(
+      <button
+        onClick={(e) => {
+          setData(data + e.target.value);
+        }}
+        value={item}
+        key={item}
+      >
+        {" "}
+        {item}
+      </button>
+    );
+  });
+  return (
+    <div className="outline">
+      {" "}
+      <div className="input">{data}</div>
+      <div className="digits">{calcBtns}</div>
+      {/* First Grid */}
+      <div className="First subgrid">
+        <button onClick={() => setData(data.substr(0, data.length - 1))}>
           Clear
         </button>
-        <button onClick={()=> setData("")}>
-          AC
-        </button>
+        <button onClick={() => setData("")}>AC</button>
       </div>
-        {/* Second Grid */}
-      <div className="subgrid">
-        <button onClick={e=> setData(data + e.target.value)} value="+">
+      {/* Second Grid */}
+      <div className="Second subgrid">
+        <button onClick={(e) => setData(data + e.target.value)} value="+">
           +
         </button>
-        <button onClick={e=> setData(data + e.target.value)} value="-">
+        <button onClick={(e) => setData(data + e.target.value)} value="-">
           -
         </button>
-        <button onClick={e=> setData(data + e.target.value)} value="*">
+        <button onClick={(e) => setData(data + e.target.value)} value="*">
           *
         </button>
-        <button onClick={e=> setData(data + e.target.value)} value="/">
+        <button onClick={(e) => setData(data + e.target.value)} value="/">
           /
         </button>
         {/* Final Score button */}
@@ -51,19 +51,20 @@ function Calculator(){
             try {
               setData(
                 String(eval(data)).length > 3 &&
-                String(eval(data)).includes(".")
-                ? String(eval(data).toFixed(4))
-                : String(eval(data))
+                  String(eval(data)).includes(".")
+                  ? String(eval(data).toFixed(4))
+                  : String(eval(data))
               );
             } catch (err) {
               console.log(err);
             }
           }}
-          value="=">
+          value="="
+        >
           =
         </button>
       </div>
-  </div>  
+    </div>
   );
 }
 
